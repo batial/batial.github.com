f852938bad46fd7c8db853f1110252153646dd66
Sebastián Debesa <sebastian.debesa@hotmail.com>
2022-08-16 22:51:23 -0300

    agregué el login de la web

--- js/index.js
+++ js/index.js
@@ -1,14 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
+const boton = document.getElementById('ingresar');
+const email = document.getElementById('email');
+const pass = document.getElementById('pass');
+
+
+boton.addEventListener('click',(e)=> {
+    e.preventDefault();
+    
+    const data = {
+        email: email.value,
+        pass: pass.value,
+    }
+    if (data.email !== '' && data.pass !== '' ){
+        window.location.href = "home.html";
+    } else {
+        alert('Por favor, asegurese de completar las casillas correctamente.')
+    }
+
 });
+
+/*const email = document.getElementById('email');
+const pass = document.getElementById('pass');*/
+
+
+
+/*if (document.form.email.value !=='' && document.form.pass.value !== '') {
+    window.location.replace("home.html");
+} else {
+    window.location.replace("home.html");
+}
+*/
