902854132718f950b39a34fd3257a18558e95afc
Sebastián Debesa <sebastian.debesa@hotmail.com>
2022-08-27 15:16:36 -0300

    funcionalidad entrega-2-1

--- js/index.js
+++ js/index.js
@@ -1,19 +1,30 @@
-const boton = document.getElementById('ingresar');
-const email = document.getElementById('email');
-const pass = document.getElementById('pass');
+document.addEventListener("DOMContentLoaded",()=>{
+    const boton = document.getElementById('ingresar');
+    const email = document.getElementById('email');
+    const pass = document.getElementById('pass');
 
-
-boton.addEventListener('click',(e)=> {
-    e.preventDefault();
+    var userData; 
+    if (boton !== null){ // condicional para evitar que se siga cargando el boton en el home <--
+        boton.addEventListener('click',async (e)=> {
+            e.preventDefault();
+            const Data = {
+                email: email.value,
+                pass: pass.value,
+            }
+            if (Data.email !== '' && Data.pass !== '' ){
+                window.location.href = "home.html";
+            } else {
+                alert('Por favor, asegurese de completar las casillas correctamente.')
+            }
+            localStorage.setItem('userName',Data.email)
+        });
+    }
     
-    const data = {
-        email: email.value,
-        pass: pass.value,
-    }
-    if (data.email !== '' && data.pass !== '' ){
-        window.location.href = "home.html";
-    } else {
-        alert('Por favor, asegurese de completar las casillas correctamente.')
-    }
+    //Estructura para mostrar el id del usuario en home
+    const userName = document.getElementById('userName');
+    const getKeyName = local.getItem('userName')
 
+    if (userName !== null && getKeyName !== null){ // condicional para evitar problemas fuera del home.
+        userName.innerHTML = `<a class="nav-link" href="">${getKeyName}</a>`;
+    }
 });
