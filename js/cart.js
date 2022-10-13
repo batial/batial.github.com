e2c71a0117b5e80d89f0b7a690e1f5a6e3b5ef41
Sebastián Debesa <sebastian.debesa@hotmail.com>
2022-10-13 18:20:28 -0300

    parte 3 entregable 5

--- js/cart.js
+++ js/cart.js
@@ -1,18 +1,21 @@
 const userID = 25801
 
 function getCartProducts(data){
-    HTMLContentToAppend = '';
+    let HTMLContentToAppend = '';
+    let artCount = 0;
     data.forEach(element => {
         HTMLContentToAppend += `
         <tr>
             <th scope="row"><img src="${element.image}" width="50rem" alt=""></th>
             <td>${element.name}</td>
             <td>${element.currency} ${element.unitCost}</td>
-            <td><input type="number" value="${element.count ?? 1}" min="1" max="100" oninput="getSubTotal(${element.unitCost},${element.count})"></td>
-            <td>${element.currency} ${element.unitCost * element.count}</td>
+            <td><input id=cantN${artCount} type="number" value="${element.count ?? 1}" min="1" max="100" oninput="getSubTotal(${element.unitCost},${artCount})"></td>
+            <td>${element.currency} <span id=itemN${artCount}>${element.unitCost * element.count}</span></td>
         </tr>
         `
+        artCount++
     });
+
     return HTMLContentToAppend;
 }
 
@@ -21,13 +24,14 @@ document.addEventListener('DOMContentLoaded',async ()=>{
     const cartData = await getJSONData(CART_INFO_URL + userID + EXT_TYPE);
 
     itemsContainer.innerHTML = getCartProducts(cartData.data.articles);
-
-
     console.log(cartData.data);
 })
 
-function getSubTotal(unitCost,value){
-    console.log("el costo es " + unitCost);
-    console.log('el value es' + value);
+function getSubTotal(unitCost,id){
+    let multiplo = document.getElementById('cantN'+id).value;
+    let subtotal = unitCost * multiplo;
 
+    console.log("el costo es " + unitCost);
+    console.log('el value es' + multiplo);
+    document.getElementById('itemN'+id).innerHTML = subtotal;
 }
