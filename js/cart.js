4b11222856b2f67ba58efbc689c6b41bc2e32edf
Sebastián Debesa <sebastian.debesa@hotmail.com>
2022-10-13 17:26:56 -0300

    parte 2 de entregable 5

--- js/cart.js
+++ js/cart.js
@@ -8,8 +8,8 @@ function getCartProducts(data){
             <th scope="row"><img src="${element.image}" width="50rem" alt=""></th>
             <td>${element.name}</td>
             <td>${element.currency} ${element.unitCost}</td>
-            <td><input type="number" value="1" min="1" max="100"></td>
-            <td>${element.currency} ${element.unitCost}</td>
+            <td><input type="number" value="${element.count ?? 1}" min="1" max="100" oninput="getSubTotal(${element.unitCost},${element.count})"></td>
+            <td>${element.currency} ${element.unitCost * element.count}</td>
         </tr>
         `
     });
@@ -21,5 +21,13 @@ document.addEventListener('DOMContentLoaded',async ()=>{
     const cartData = await getJSONData(CART_INFO_URL + userID + EXT_TYPE);
 
     itemsContainer.innerHTML = getCartProducts(cartData.data.articles);
+
+
     console.log(cartData.data);
 })
+
+function getSubTotal(unitCost,value){
+    console.log("el costo es " + unitCost);
+    console.log('el value es' + value);
+
+}
