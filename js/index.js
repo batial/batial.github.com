6932ed18d78754456489bb5ffaa5f9d9e84696ad
Sebastián Debesa <sebastian.debesa@hotmail.com>
2023-05-23 14:57:08 -0300

    login revisado ahora con funcionalidades de bootrstap 5.0

--- js/index.js
+++ js/index.js
@@ -1,21 +1,16 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    const boton = document.getElementById('ingresar');
-    const email = document.getElementById('email');
-    const pass = document.getElementById('pass');
 
-    if (boton !== null){ // condicional para evitar que se siga cargando el boton fuera del index.html <--
-        boton.addEventListener('click',async (e)=> {
-            e.preventDefault();
-            const Data = {
-                email: email.value,
-                pass: pass.value,
-            }
-            if (Data.email !== '' && Data.pass !== '' ){
-                window.location.href = "home.html";
-            } else {
-                alert('Por favor, asegurese de completar las casillas correctamente.')
-            }
-            localStorage.setItem('userEmail',Data.email);
-        });
-    }
+document.addEventListener("DOMContentLoaded",()=>{
+    const email = document.getElementById('email');
+    const form = document.querySelector('.needs-validation');
+    
+    form.addEventListener('submit', function (event) {
+        event.preventDefault()
+        if (!form.checkValidity()) {
+            event.stopPropagation()
+        } else{
+            window.location.href = "home.html";
+            localStorage.setItem('userEmail', email.value);  
+        }
+        form.classList.add('was-validated');
+        }, false)
 });
